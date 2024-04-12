97f9327f7d9f03a22ca7781b538f207a2493d870
jeek <zkkysqs@gmail.com>
2024-04-12 15:53:50 +0800

    feat: update ButtonDemo

--- lib/contents/button.dart
+++ lib/contents/button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ButtonDemo extends StatelessWidget {
   const ButtonDemo({super.key});
@@ -9,8 +10,8 @@ class ButtonDemo extends StatelessWidget {
     return Wrap(children: [
       TextButton(
           onPressed: () {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Yay! A SnackBar!')));
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Yay! A SnackBar!')));
           },
           child: const Text('文本按钮-默认主题')),
       const TextButton(onPressed: null, child: Text('文本按钮禁用')),
@@ -27,7 +28,9 @@ class ButtonDemo extends StatelessWidget {
           },
           child: const Text('文本按钮-修改颜色', style: TextStyle(color: Colors.blue))),
       TextButton(
-        onPressed: () {},
+        onPressed: () {
+          launchUrl(Uri.parse('https://flutter.cn/'));
+        },
         child: const Text('文本按钮-模拟a标签',
             style: TextStyle(
                 color: Colors.blue,
@@ -36,8 +39,7 @@ class ButtonDemo extends StatelessWidget {
       ),
       const Divider(),
       TextButton.icon(
-          onPressed: () {
-          },
+          onPressed: () {},
           icon: const Icon(Icons.star),
           label: const Text('图标按钮')),
       TextButton.icon(
@@ -45,15 +47,81 @@ class ButtonDemo extends StatelessWidget {
             Navigator.pop(context);
           },
           icon: const Icon(Icons.home),
-          label: const Text('图标按钮')),
+          label: const Text('回到home')),
+      TextButton.icon(
+          onPressed: null,
+          icon: const Icon(Icons.not_interested),
+          label: const Text('禁用')),
       const Divider(),
       ElevatedButton(onPressed: () {}, child: const Text('阴影按钮1')),
+      const SizedBox(width: 10),
       ElevatedButton(
-          onPressed: () {
-          },
+          onPressed: () {},
           style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              backgroundColor: WidgetStateProperty.all(Colors.blue)),
           child: const Text('阴影按钮2', style: TextStyle(color: Colors.white))),
+      const SizedBox(width: 10),
+      const ElevatedButton(onPressed: null, child: Text('禁用')),
+      const Divider(),
+      SizedBox(
+        height: 100,
+        width: double.infinity,
+        child: FloatingActionButton(
+          elevation: 20,
+          tooltip: '浮动按钮, 改变尺寸',
+          onPressed: () {},
+          child: SizedBox(
+            height: double.infinity,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  gradient:
+                      LinearGradient(colors: [Colors.blue, Colors.green])),
+              child: const Row(
+                children: [
+                  Icon(Icons.sunny, size: 24, color: Colors.yellow),
+                  Spacer(),
+                  Text('浮动按钮, 改变尺寸')
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      const Divider(),
+      OverflowBar(
+        spacing: 20,
+        children: [
+          OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+              ),
+              child: const Text(
+                "边框按钮1",
+                style: TextStyle(color: Colors.redAccent),
+              )),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+                side: const BorderSide(width: 1, color: Colors.red)),
+            onPressed: () {},
+            child: const Text(
+              "边框按钮2",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          const OutlinedButton(
+            onPressed: null,
+            child: Text(
+              "禁用",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ],
+      )
     ]);
   }
 }
